3d7a9064917b3a6424e9313410404f50c6e0a6c8
egor <egornick98@gmail.com>
2026-04-14 10:16:27 +0300

    add: first version for code

--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -6,7 +6,7 @@ type TFetcher = {
 // Обычно хранят в env файле, но в рамках этой задачи я объявил ее здесь
 const BaseURL = 'https://jsonplaceholder.typicode.com/';
 
-export async function fetcher({url}: TFetcher) {
+export async function fetcher({ url }: TFetcher) {
     const response = await fetch(`${BaseURL}${url}`);
 
     if (!response.ok) {
